cef25b5a93d3ff996a3c417e6444acd43bb05d20
Dibyendu Das <43660480+Dibyendu1000@users.noreply.github.com>
2019-10-12 06:17:48 +0530

    Update: Add Time left in "Hour<space>Minute<space>Seconds" for the sale

--- bot_script2.py
+++ bot_script2.py
@@ -1,18 +1,19 @@
 import time
 import pyautogui as pg
-def tinsec(hr,mn):
+def tinsec(hr,mn,s):
     sec=time.time()
-    #sec+=hr*3600
+    sec+=hr*3600
     sec+=mn*60
+    sec+=s
     return sec
-def hello(hr,mn):
-    dest=tinsec(hr,mn)
+def hello(hr,mn,sec):
+    dest=tinsec(hr,mn,sec)
     while(dest-time.time()>2):
         print(dest-time.time())
     else:
         dest2=time.time()        
-        while(time.time()-dest2<4):
+        while(time.time()-dest2<10):
             pg.moveTo(x=622,y=839, duration=1)
             pg.click(interval=0.1)
-        
-hello(0,0.10)
+ask_time=tuple(map(float,input("Enter Time Left:").split(" ")))
+hello(ask_time[0],ask_time[1],ask_time[2])
